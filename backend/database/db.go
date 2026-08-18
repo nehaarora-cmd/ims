32060da9db9402b7c054d64fe539b19147a28f71
Neha Arora <nehaarora.freelance.dev@gmail.com>
2026-08-18 17:04:45 +0530

    main(fix): use database url
    Signed-off-by: Neha Arora <neha.freelance.dev@gmail.com>

--- backend/database/db.go
+++ backend/database/db.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,14 +11,10 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
