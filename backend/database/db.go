c1b6a6597a0e5a1cf30e590a1023b1a4174b7525
Neha Arora <nehaarora.freelance.dev@gmail.com>
2026-08-20 17:29:18 +0530

    main(fix): set preparestmt to false to prevent nul
    Disabled PrepareStatement to prevent backend from
     returning null.
    
    Signed-off-by: Neha Arora <neha.freelance.dev@gmail.com>

--- backend/database/db.go
+++ backend/database/db.go
@@ -17,7 +17,9 @@ func Connect() {
 	}
 
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
