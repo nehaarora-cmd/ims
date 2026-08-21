85cea54e2b9602de72ae846a1d1fcbc43d89499c
Neha Arora <nehaarora.freelance.dev@gmail.com>
2026-08-21 14:10:19 +0530

    main(fix): add content length check aswell.
    Signed-off-by: Neha Arora <neha.freelance.dev@gmail.com>

--- backend/middleware/max_byte_middleware.go
+++ backend/middleware/max_byte_middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,7 +10,12 @@ import (
 func MaxBytesMiddleware(maxBytes int64) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Wrap the request body with a limit
+			if r.ContentLength > maxBytes {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusRequestEntityTooLarge)
+				json.NewEncoder(w).Encode(map[string]string{"error": "Request body too large. Max 5 MB allowed."})
+				return
+			}
 			r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
 			next.ServeHTTP(w, r)
 		})
